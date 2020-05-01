fb69f95d6c0858a9936793e9ae93cc61f06c5a0f
marcelats <marcelatshinzato@gmail.com>
2020-05-01 17:03:57 -0300

    Update Wizard.java

--- Wizard.java
+++ Wizard.java
@@ -1,2 +1,150 @@
 //essa classe pertence ao pacote battlegame
 package battlegame;
+
+//classe Warrior, herdeira de Character
+public class Wizard extends Character
+{
+    //instância privada
+    private int wand;
+        
+    //métodos
+    //construtor
+    public Wizard(String name)
+    {
+        super(name);
+        //os valores serão alterados posteriormente, é só para não deixar em branco
+        this.maxPower = 20; 
+        this.maxHp = 35;
+        this.power = 20;
+        this.hp = 35;
+        this.level = 1;
+        this.xp = 0;
+        this.atk = 1;
+        this.def = 2;
+        this.speed = 4;
+        this.intel = 5;
+    }
+    
+    //getters
+    public int getMaxPower()
+    {
+        return this.maxPower;
+    }
+    
+    public int getMaxHp()
+    {
+        return this.maxHp;
+    }
+    
+    public int getPower()
+    {
+        return this.power;
+    }
+    
+    public int getHp()
+    {
+        return this.hp;
+    }
+    
+    public int getLevel()
+    {
+        return this.level;
+    }
+    
+    public int getXp()
+    {
+        return this.xp;
+    }
+    
+    public int getAttack()
+    {
+        return this.atk;
+    }
+    
+    public int getDefense()
+    {
+        return this.def;
+    }
+    
+    public int getSpeed()
+    {
+        return this.speed;
+    }
+    
+    public int getIntel()
+    {
+        return this.intel;
+    }
+    
+    public String getName()
+    {
+        return this.name;
+    }
+    
+    public int getWand()
+    {
+        return this.wand;
+    }
+        
+    //setters
+    public void setMaxPower(int maxPower)
+    {
+       this.maxPower = maxPower;
+    }
+    
+    public void setMaxHp(int maxHp)
+    {
+        this.maxHp = maxHp;
+    }
+    
+    public void setPower(int power)
+    {
+        if(power > this.maxPower) this.power = this.maxPower;
+        else this.power = power;
+    }
+    
+    public void setHp(int hp)
+    {
+        if(hp > this.hp) this.hp = this.maxHp;
+        else this.hp = hp;
+    }
+    
+    public void levelUp()
+    {
+        this.level +=1;
+        //TO DO: esse método irá chamar outros setters indicando quais valores devem ser atualizados
+    }
+    
+    public void setXp(int xp)
+    {
+        this.xp = xp;
+        //TO DO: esse método irá chamar levelUp dependendo do valor de xp
+    }
+    
+    public void setAttack(int atk)
+    {
+        this.atk = atk;
+    }
+    
+    public void setDefense(int def)
+    {
+        this.def = def;
+    }
+    
+    public void setSpeed(int speed)
+    {
+        this.speed = speed;
+    }
+    
+    public void setIntel(int intel)
+    {
+        this.intel = intel;
+    }
+    
+    public void setWand(int wand)
+    {
+        this.wand = wand;
+    }
+    
+    //os outros métodos serão implementados posteriormente
+}
