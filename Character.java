a3a1034551b1a0ccc3ed19b0b5a03f5d21de985f
marcelats <marcelatshinzato@gmail.com>
2020-05-01 16:58:49 -0300

    Update Character.java

--- Character.java
+++ Character.java
@@ -1,48 +1,62 @@
 //essa classe pertence ao pacote battlegame
 package battlegame;
 
-//classe abstrata Character
-public abstract class Character
+//classe abstrata Character, implementa Cloneable
+public abstract class Character implements Cloneable
 {
-    //instâncias
-    int maxPower, maxHp, power, hp, level, xp, atk, def, speed, intel;
+    //instâncias são protected pois precisam ser acessadas pelas classes herdeiras
+    protected int maxPower, maxHp, power, hp, level, xp, atk, def, speed, intel;
         //instância que não será modificada
         final String name;
         
     //métodos
     //construtor
     //apenas setará o nome, pois o construtor de cada classe de personagem fará o resto
-    Character(String name)
+    public Character(String name)
     {
         this.name = name;
     }
     
+    //construtor de clone
+    public Character clone() throws CloneNotSupportedException
+    {
+        try 
+        {
+            return (Character)super.clone();
+        }
+        catch(CloneNotSupportedException error) 
+        {
+            System.out.println("Ocorreu um erro ao tentar iniciar o jogo.");
+            return null;
+        }
+    }
+    
     //getters
     //todos abstratos, ou seja, serão implementados apenas nas classes herdeiras
-    abstract int getMaxPower();
-    abstract int getMaxHp();
-    abstract int getPower();
-    abstract int getHp();   
-    abstract int getLevel();
-    abstract int getXp();
-    abstract int getAttack();
-    abstract int getDefense();
-    abstract int getSpeed();
-    abstract int getIntel();
-    abstract String getName();
+    public abstract int getMaxPower();
+    public abstract int getMaxHp();
+    public abstract int getPower();
+    public abstract int getHp();   
+    public abstract int getLevel();
+    public abstract int getXp();
+    public abstract int getAttack();
+    public abstract int getDefense();
+    public abstract int getSpeed();
+    public abstract int getIntel();
+    public abstract String getName();
         
     //setters
     //todos abstratos, ou seja, serão implementados apenas nas classes herdeiras
-    abstract int setMaxPower();
-    abstract int setMaxHp();
-    abstract int setPower();
-    abstract int setHp();   
-    abstract int setLevel();
-    abstract int setXp();
-    abstract int setAttack();
-    abstract int setDefense();
-    abstract int setSpeed();
-    abstract int setIntel();
+    public abstract void setMaxPower(int maxPower);
+    public abstract void setMaxHp(int maxHp);
+    public abstract void setPower(int power);
+    public abstract void setHp(int hp);   
+    public abstract void levelUp();
+    public abstract void setXp(int xp);
+    public abstract void setAttack(int atk);
+    public abstract void setDefense(int def);
+    public abstract void setSpeed(int speed);
+    public abstract void setIntel(int intel);
     
     //os outros métodos serão implementados posteriormente
 }
