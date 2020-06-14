a42030d9afcc51a33aef134745ab55c0089cf7e4
marcelats <marcelatshinzato@gmail.com>
2020-06-14 17:07:46 -0300

    Update Character.java

--- Character.java
+++ Character.java
@@ -32,31 +32,112 @@ public abstract class Character implements Cloneable
     }
     
     //getters
-    //todos abstratos, ou seja, serão implementados apenas nas classes herdeiras
-    public abstract int getMaxPower();
-    public abstract int getMaxHp();
-    public abstract int getPower();
-    public abstract int getHp();   
-    public abstract int getLevel();
-    public abstract int getXp();
-    public abstract int getAttack();
-    public abstract int getDefense();
-    public abstract int getSpeed();
-    public abstract int getIntel();
-    public abstract String getName();
-        
-    //setters
-    //todos abstratos, ou seja, serão implementados apenas nas classes herdeiras
-    public abstract void setMaxPower(int maxPower);
-    public abstract void setMaxHp(int maxHp);
-    public abstract void setPower(int power);
-    public abstract void setHp(int hp);   
-    public abstract void levelUp();
-    public abstract void setXp(int xp);
-    public abstract void setAttack(int atk);
-    public abstract void setDefense(int def);
-    public abstract void setSpeed(int speed);
-    public abstract void setIntel(int intel);
+    //métodos default
+    public int getMaxPower()
+    {
+        return this.maxPower;
+    }
     
+    public int getMaxHp()
+    {
+        return this.maxHp;
+    }
+    
+    public int getPower()
+    {
+        return this.power;
+    }
+    
+    public int getHp()
+    {
+        return this.hp;
+    }
+    
+    public int getLevel()
+    {
+        return this.level;
+    }
+    
+    public int getXp()
+    {
+        return this.xp;
+    }
+    
+    public int getAttack()
+    {
+        return this.atk;
+    }
+    
+    public int getDefense()
+    {
+        return this.def;
+    }
+    
+    public int getSpeed()
+    {
+        return this.speed;
+    }
+    
+    public int getIntel()
+    {
+        return this.intel;
+    }
+    
+    public String getName()
+    {
+        return this.name;
+    }
+    
+    //setters
+    //métodos default
+    public void setMaxPower(int maxPower)
+    {
+       this.maxPower += maxPower;
+    }
+    
+    public void setMaxHp(int maxHp)
+    {
+        this.maxHp += maxHp;
+    }
+    
+    public void setPower(int power)
+    {
+        if(this.power + power > this.maxPower) this.power = this.maxPower;
+        else this.power += power;
+    }
+    
+    public void setHp(int hp)
+    {
+        if(this.hp + hp > this.maxHp) this.hp = this.maxHp;
+        else this.hp += hp;
+    }
+    
+    public void setXp(int xp)
+    {
+        this.xp += xp;
+        //TO DO: esse método irá chamar levelUp dependendo do valor de xp
+    }
+    
+    public void setAttack(int atk)
+    {
+        this.atk += atk;
+    }
+    
+    public void setDefense(int def)
+    {
+        this.def += def;
+    }
+    
+    public void setSpeed(int speed)
+    {
+        this.speed += speed;
+    }
+    
+    public void setIntel(int intel)
+    {
+        this.intel += intel;
+    }
+    //método abstract
+    public abstract void levelUp();
     //os outros métodos serão implementados posteriormente
 }
