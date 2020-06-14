013acb90406750dde676a0ce68a00c7eb795755c
marcelats <marcelatshinzato@gmail.com>
2020-06-14 18:28:15 -0300

    Update Game.java

--- Game.java
+++ Game.java
@@ -3,6 +3,9 @@ package battlegame;
 
 //uso de ArrayList
 import java.util.ArrayList;
+//uso da leitura do arquivo
+import java.util.Scanner;
+import java.io.*;
 
 //classe Game
 public class Game
@@ -45,14 +48,59 @@ public class Game
         this.maxBattles = maxBattles;
         this.death = death;
         //TO DO: ler um arquivo de texto para criar allCharacters e um método para gerar playerOneDeck e playerTwoDeck
+        //cria-se um ArrayList auxiliar
+        ArrayList<Character> aux = new ArrayList<Character>();
+        //é preciso considerar a exceção de o arquivo não ser encontrado, então tenta se executar o código e 
+        //caso não se encontre o arquivo, imprime-se mensagem de erro e retorna-se um ArrayList nulo
+        try
+        {
+            //usa-se um objeto Scanner para ler o arquivo, que é acessado por um objeto File, que acessa o arquivo CharactersFile.txt
+            Scanner sc = new Scanner(new File("CharactersFile.txt"));
+            //os dados serão lidos enquanto não se chegar ao fim do arquivo
+            while (sc.hasNextLine())
+            {
+                //lê-se uma string que será o nome
+                String name = sc.nextLine();
+                //lê-se um inteiro que será o valor inicial de ataque
+                int atk = sc.nextInt();
+                //como o próximo dado está na próxima linha e o método nextInt() não considera quebra de linha, 
+                //o fazemos chamando o método nextLine() sem atribuí-lo a variável alguma
+                sc.nextLine();
+                //lê-se um inteiro que será o valor inicial de defesa
+                int def = sc.nextInt();
+                //como o próximo dado está na próxima linha e o método nextInt() não considera quebra de linha, 
+                //o fazemos chamando o método nextLine() sem atribuí-lo a variável alguma
+                sc.nextLine();
+                //lê-se um inteiro que será o valor inicial de velocidade
+                int speed = sc.nextInt();
+                //como o próximo dado está na próxima linha e o método nextInt() não considera quebra de linha, 
+                //o fazemos chamando o método nextLine() sem atribuí-lo a variável alguma
+                sc.nextLine();
+                //lê-se um inteiro que será o valor inicial de inteligência
+                int intel = sc.nextInt();
+                //como o próximo dado está na próxima linha e o método nextInt() não considera quebra de linha, 
+                //o fazemos chamando o método nextLine() sem atribuí-lo a variável alguma
+                sc.nextLine();
+                //lê-se um inteiro que indicará qual a classe do personagem
+                int classID = sc.nextInt();
+                if(classID == 1) aux.add(new Warrior(name, atk, def, speed, intel));
+                else if(classID == 2) aux.add(new Wizard(name, atk, def, speed, intel));
+                else if(classID == 3) aux.add(new Archer(name, atk, def, speed, intel));
+                else aux.add(new Healer(name, atk, def, speed, intel));
+            }
+        }
+        //caso o arquivo não seja encontrado, imprime-se "erro" e retorna-se um ArrayList nulo
+        catch (FileNotFoundException error)
+        {
+            System.out.println("Error");
+        }
+        this.allCharacters = aux;
         //só para não dar erro de compilação, vamos setar como null
-        this.allCharacters = null;
         this.playerOneDeck = null;
         this.playerTwoDeck = null;
         this.playerOneParty = null;
         this.playerTwoParty = null;
     }
-    
     //getters
     public int getPlayerOneScore()
     {
