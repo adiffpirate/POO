34707ea89aa55fc13d19af6ff010cdc4bb7338d1
marcelats <marcelatshinzato@gmail.com>
2020-06-14 23:15:37 -0300

    Update Archer.java

--- Archer.java
+++ Archer.java
@@ -1,25 +1,15 @@
 //essa classe pertence ao pacote battlegame
-package battlegame;
+//package battlegame;
 
 //classe Archer, herdeira de Character
 public class Archer extends Character
 {
     //métodos
     //construtor
-    public Archer(String name)
+    public Archer(String name, int atk, int def)
     {
-        super(name);
-        //os valores serão alterados posteriormente, é só para não deixar em branco
-        this.maxPower = 20; 
-        this.maxHp = 45;
-        this.power = 20;
-        this.hp = 45;
-        this.level = 1;
-        this.xp = 0;
-        this.atk = 3;
-        this.def = 4;
-        this.speed = 2;
-        this.intel = 1;
+        super(name, atk, def);
+        this.hp = 20;
     }
     
     public void levelUp()
@@ -27,5 +17,19 @@ public class Archer extends Character
         this.level +=1;
         //TO DO: esse método irá chamar outros setters indicando quais valores devem ser atualizados
     }
+    
+    public void showActions(int power)
+    {
+        if(power > 9)
+        {
+            System.out.println("(0) Mirar (custo: 5");
+            System.out.println("(1) Horda de flechas (custo: 10");
+        }
+        else
+        {
+            System.out.println("(0) Mirar (custo: 5");
+        }
+    }
+    
     //os outros métodos serão implementados posteriormente
 }
