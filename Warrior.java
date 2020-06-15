9c43271c9de4ea352764cf4c88b54d6ceddcf14b
marcelats <marcelatshinzato@gmail.com>
2020-06-14 23:16:06 -0300

    Update Warrior.java

--- Warrior.java
+++ Warrior.java
@@ -1,31 +1,35 @@
 //essa classe pertence ao pacote battlegame
-package battlegame;
+//package battlegame;
 
 //classe Warrior, herdeira de Character
 public class Warrior extends Character
 {
     //métodos
     //construtor
-    public Warrior(String name)
+    public Warrior(String name, int atk, int def)
     {
-        super(name);
-        //os valores serão alterados posteriormente, é só para não deixar em branco
-        this.maxPower = 20; 
-        this.maxHp = 40;
-        this.power = 20;
-        this.hp = 40;
-        this.level = 1;
-        this.xp = 0;
-        this.atk = 5;
-        this.def = 3;
-        this.speed = 1;
-        this.intel = 1;
+        super(name, atk, def);
+        this.maxHp = 15;
+        this.hp = 15;
     }
         
     public void levelUp()
     {
         this.level +=1;
         //TO DO: esse método irá chamar outros setters indicando quais valores devem ser atualizados
+    }
+    
+    public void showActions(int power)
+    {
+        if(power > 9)
+        {
+            System.out.println("(0) Golpear (custo: 5");
+            System.out.println("(1) Massacre (custo: 10");
+        }
+        else
+        {
+            System.out.println("(0) Golpear (custo: 5");
+        }
     }
     //os outros métodos serão implementados posteriormente
 }
